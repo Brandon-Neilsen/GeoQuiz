ef48c40d6aa8a001785147740f337fb0b5de5dea
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-10-05 11:30:35 -0700

    GeoQuiz Version 2.2.2
    Chapter 5 has been completed, including the challenge at the end.

--- app/src/main/java/com/example/brandon/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/brandon/geoquiz/CheatActivity.java
@@ -1,20 +1,58 @@
 package com.example.brandon.geoquiz;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class CheatActivity extends AppCompatActivity
 {
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.example.brandon.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "com.example.brandon.geoquiz.answer_shown";
+    public static final String KEY_CHEAT = "cheat";
+    private boolean mAnswerIsTrue;
+    private boolean mIsAnswerShown = false;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+
+    private void setAnswerShownResult(boolean isAnswerShown)
+    {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        setAnswerShownResult(mIsAnswerShown);//Answer will not be shown until the user presses the button
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue)
+                    mAnswerTextView.setText(R.string.true_button);
+                else
+                    mAnswerTextView.setText(R.string.false_button);
+                mIsAnswerShown = true;
+                setAnswerShownResult(mIsAnswerShown);
+            }
+        });
+    }//end onCreate()
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_CHEAT, mIsAnswerShown);
     }
-}
+}//end CheatActivity class
--- app/src/main/java/com/example/brandon/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/brandon/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.example.brandon.geoquiz;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,8 +17,10 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
     //private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
 
@@ -30,6 +33,13 @@ public class QuizActivity extends AppCompatActivity {
                     new TrueFalse(R.string.question_americas, true),
                     new TrueFalse(R.string.question_asia, true)
             };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(data == null)
+            return;
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+    }//end onActivityResult
 
     private void updateQuestion()
     {
@@ -42,11 +52,15 @@ public class QuizActivity extends AppCompatActivity {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
         int messageResId;
-
-        if (userPressedTrue == answerIsTrue)
-            messageResId = R.string.correct_toast;
+        if (mIsCheater)
+            messageResId = R.string.judgement_toast;
         else
-            messageResId = R.string.incorrect_toast;
+        {
+            if (userPressedTrue == answerIsTrue)
+                messageResId = R.string.correct_toast;
+            else
+                messageResId = R.string.incorrect_toast;
+        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }//end checkAnswer(boolean)
 
@@ -100,6 +114,18 @@ public class QuizActivity extends AppCompatActivity {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(i, 0);
+            }
+
+        });
+
         updateQuestion();
 
     }//end onCreate(Bundle)
@@ -109,7 +135,8 @@ public class QuizActivity extends AppCompatActivity {
     {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-    }
+        savedInstanceState.putBoolean(KEY_INDEX, mIsCheater);
+    }//end onSaveInstanceState
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
